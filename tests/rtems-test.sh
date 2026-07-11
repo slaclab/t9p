554020e0996c8221df78440671a70702011fc5d8
Jeremy Lorelli <lorelli@slac.stanford.edu>
2026-07-10 18:50:12 -0700

    tests: Removing tcpsrv integration, other tweaks

--- tests/rtems-test.sh
+++ tests/rtems-test.sh
@@ -47,18 +47,13 @@ done
 # diod running on host port 10002
 BSP_ARGS="$BSP_ARGS --console=/dev/com1 -u $(whoami) -a $PWD/fs -m $PWD/mnt 10.0.2.2!10002"
 
-if [[ "$(echo $TARGET | cut -d '-' -f1)" == "rtems4" ]]; then
+if [[ "$(echo $TARGET | cut -d '-' -f2)" == "rtems" ]]; then
     NETDEV=ne2k_pci
 else
     NETDEV=e1000
 fi
 echo "NETDEV=$NETDEV"
 
-gcc -o tcpsrv tcpsrv.c -lc
-./tcpsrv &
-
 $LIMIT qemu-system-$ARCH $QEMU_ARGS -no-reboot -m 128M -serial mon:stdio -nographic \
     -device $NETDEV,netdev=em0 -netdev user,id=em0,hostfwd=tcp::10003-:10003,hostfwd=tcp::1234-:1234,hostfwd=udp::5000-:5000 \
-    -append "$BSP_ARGS" -kernel $PWD/../build-cmake/build-$TARGET/t9p_rtems_test
-
-kill $(jobs -p)
+    -append "$BSP_ARGS" -kernel $PWD/../build/$TARGET/t9p_rtems_test
